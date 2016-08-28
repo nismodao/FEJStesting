78a1d3a309b21bd7e6f74161ba8394e64123852d
Nam Dao <nam.d.dao@gmail.com>
2016-08-28 15:33:14 -0700

    change color to black

--- tests/checkDivBg.js
+++ tests/checkDivBg.js
@@ -1,7 +1,7 @@
 describe('getDivBg', function() {
     var d = document.querySelector('.box');
 
-    it('Should be teal', function() {
-        expect(d.style.backgroundColor).toBe('teal');
+    it('Should be black', function() {
+        expect(d.style.backgroundColor).toBe('black');
     });
 });
